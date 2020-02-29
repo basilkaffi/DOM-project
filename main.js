d5ebc9f4ab1ed096d98a8020ff4f664f1ec707f1
basil kaffi <basilarr7@gmail.com>
2020-02-29 15:57:33 +0700

    sector about

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-const text = ["Website", "Apps", "Game"]
-    let count = 0
-    let index = 0
-    let anggotaText = ""
-    let huruf = ""
-    if(count === text.length) {
-        count = 0
-    }
+//tampilan mengetik
+
+const text = ["Website", "Games", "Apps"]
+let count = 0
+let index = 0
+let textMember = ""
+let letter = ""
+if(count === text.length) {
+    count = 0
+}
 (function type(){
     if(count === text.length) {
         count = 0
     }
-    anggotaText = text[count]
-    huruf = anggotaText.slice(0,index++)
-    document.querySelector(".typing").textContent = huruf
-    if(huruf.length === anggotaText.length){
+    textMember = text[count]
+    letter = textMember.slice(0,index++)
+    document.querySelector(".typing").textContent = letter
+    if(letter.length === textMember.length){
         count++
         index = 0
     }
     setTimeout(type,550)
 }())
+
+
+//kursor bulat
 
 var mouseCursor = document.querySelector('.cursor')
 var navLinks = document.querySelectorAll('.nav-links a')
@@ -30,13 +35,34 @@ function cursor(e){
     mouseCursor.style.left = e.pageX + "px"
 }
 
-navLinks.forEach(link => {
-    link.addEventListener('mouseover', () => {
+
+//hover nav link
+
+navLinks.forEach(function(link) {
+    link.addEventListener('mouseover', function () {
         mouseCursor.classList.add('cursor-change')
-        link.style.color = 'rgb(25,25,25)'
+        link.style.color = 'rgb(34,40,49)'
+        link.style.fontSize = '25px'
     })
-    link.addEventListener('mouseleave', () => {
+    link.addEventListener('mouseleave', function () {
         mouseCursor.classList.remove('cursor-change')
         link.style.color = 'rgb(238,238,238)'
+        link.style.fontSize = '16px'
     })
+})
+
+
+//tampilan about
+window.addEventListener('scroll', function(){
+   var appearing = document.querySelector('.appear')
+   var distanceToTop = appearing.getBoundingClientRect().top
+   var positionAppear = window.innerHeight/1.3
+   if(distanceToTop<positionAppear){
+       appearing.style.opacity = '1'
+       appearing.style.transform = 'translateY(0px)'
+   }
+   else if(distanceToTop>positionAppear){
+    appearing.style.opacity = '0'
+    appearing.style.transform = 'translateY(20px)'
+}
 })
