145e3801f4e0cecb32bf035dc6ec09ea84d4479e
basil kaffi <basilarr7@gmail.com>
2020-02-29 20:09:14 +0700

    save dulu

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-//tampilan mengetik
-
-const text = ["Website", "Games", "Apps"]
-let count = 0
-let index = 0
-let textMember = ""
-let letter = ""
-if(count === text.length) {
-    count = 0
-}
-(function type(){
-    if(count === text.length) {
-        count = 0
-    }
-    textMember = text[count]
-    letter = textMember.slice(0,index++)
-    document.querySelector(".typing").textContent = letter
-    if(letter.length === textMember.length){
-        count++
-        index = 0
-    }
-    setTimeout(type,550)
-}())
-
-
 //kursor bulat
 
 var mouseCursor = document.querySelector('.cursor')
@@ -52,17 +27,60 @@ navLinks.forEach(function(link) {
 })
 
 
-//tampilan about
-window.addEventListener('scroll', function(){
-   var appearing = document.querySelector('.appear')
-   var distanceToTop = appearing.getBoundingClientRect().top
-   var positionAppear = window.innerHeight/1.3
-   if(distanceToTop<positionAppear){
-       appearing.style.opacity = '1'
-       appearing.style.transform = 'translateY(0px)'
-   }
-   else if(distanceToTop>positionAppear){
-    appearing.style.opacity = '0'
-    appearing.style.transform = 'translateY(20px)'
+//tampilan mengetik
+
+const text = ["Website ", "Games ", "Apps "]
+let count = 0
+let index = 0
+let textMember = ""
+let letter = ""
+if(count === text.length) {
+    count = 0
 }
-})
+(function type(){
+    if(count === text.length) {
+        count = 0
+    }
+    textMember = text[count]
+    letter = textMember.slice(0,index++)
+    document.querySelector(".typing").textContent = letter
+    if(letter.length === textMember.length){
+        count++
+        index = 0
+    }
+    setTimeout(type,550)
+}())
+
+
+//tampilan appear
+
+function Appear(){
+    var appearing = document.querySelector('.appear')
+    var distanceToTop = appearing.getBoundingClientRect().top
+    var positionAppear = window.innerHeight/1.3
+    if(distanceToTop<positionAppear){
+        appearing.style.opacity = '1'
+        appearing.style.transform = 'translateY(0px)'
+    }
+    else if(distanceToTop>positionAppear*1.3){
+     appearing.style.opacity = '0'
+     appearing.style.transform = 'translateY(20px)'
+    }
+    var burger = document.querySelector('.burger')
+    var burgerExist = window.innerHeight*1.5
+    var burgerAppear = burgerExist/1.3
+    if(distanceToTop<burgerExist){
+        burger.style.display = 'block'
+    }
+    else if(distanceToTop>burgerExist){
+        burger.style.display = 'none'
+    }
+    if(distanceToTop<burgerAppear){
+        burger.style.opacity = '1'
+    }
+    else if(distanceToTop>burgerAppear){
+     burger.style.opacity = '0'
+    }
+ }
+
+ window.addEventListener('scroll', Appear)
